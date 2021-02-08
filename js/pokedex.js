1d9074226e1907b19b46dba718e4adb74a1e752d
KaiHelsen <kai@Helsen.info>
2021-02-08 17:12:05 +0100

    added extra functions to the pokedex.js to handle moves

--- js/pokedex.js
+++ js/pokedex.js
@@ -45,13 +45,50 @@ Pokemon.GetPokemonCount = async function ()
 }
 
 /**
+ * This function parses through the moves array of the pokemon and returns the names of a specified amount.
+ * @param {number} moveCount the amount of moves to return in the final array. If the desired amount is larger than the actual moves, the array will fill itself up with X es
+ * @param {boolean} selectRandom whether the moves picked from the movelist should be random. If the desired amount is larger than the actual moves, this will be ignored
+ * @returns {[]} returns an array of names
+ * @constructor
+ */
+Pokemon.prototype.GetMoves = function(moveCount = 4, selectRandom = false)
+{
+    let moveArray = [];
+    if(this.moves.length > moveCount){
+        for(let i = 0; i < moveCount; i++)
+        {
+            let index = i;
+            if(selectRandom) index = Math.random()*this.moves.length | 0;
+
+            moveArray.push(this.moves[i].move.name);
+        }
+    }
+    else{
+        for(let i = 0; i < moveCount; i++)
+        {
+            if(this.moves[i] != null)
+            {
+                moveArray.push(this.moves[i].move.name);
+            }
+            else
+            {
+                moveArray.push("X");
+            }
+        }
+    }
+
+
+    return moveArray;
+}
+
+/**
  * returns raw species data from pokeAPI about the species. mostly instrumental in fetching the evolution chain of a specific pokemon.
  * @returns {Promise<*|Response>}
  * @constructor
  */
 Pokemon.prototype.GetSpecies = async function ()
 {
-
+    let speciesData = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${this.id}/`);
     speciesData = await speciesData.json();
     // console.log(speciesData);
     return speciesData;
@@ -65,40 +102,37 @@ Pokemon.prototype.GetSpecies = async function ()
  */
 Pokemon.prototype.GetPrevEvolution = async function ()
 {
-    let speciesData = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${this.id}/`);
-    let data = await speciesData.json();
+    let data = await this.GetSpecies();
     return Pokemon.FetchPokemon(data.evolves_from_species.name);
-
-
 }
-
-// /**
-//  * !!!IN PROGRESS!!!
-//  * gets the next links in the evolution chain and returns as array of pokemon objects
-//  * @returns {Promise<*>}
-//  * @constructor
-//  */
-// Pokemon.prototype.GetNextEvolutions = async function()
-// {
-//     let data = await this.GetEvolutionChain();
-//     console.table(data.chain.evolves_to);
-//     return data.chain.evolves_to;
-// }
 
 /**
  * !!!IN PROGRESS!!!
+ * gets the next links in the evolution chain and returns as array of pokemon objects
+ * @returns {Promise<*>}
+ * @constructor
+ */
+Pokemon.prototype.GetNextEvolutions = async function ()
+{
+    let data = await this.GetEvolutionChain();
+    console.table(data.chain.evolves_to);
+    return data.chain.evolves_to;
+}
+
+/**
+ * !!!IN PROGRESS!!!
+ * !!!DOES NOT WORK PROPERLY! USE AT OWN RISK!!!
  * returns the evolution chain data of the Pokemon in question
  * TODO: rewrite this to instead return an array of pokemon objects rather than just the raw JSON data
  * @returns {Promise<*|Response>}
  * @constructor
  */
-Pokemon.prototype.GetEvolutionChain = async function ()
+Pokemon.prototype.getNextEvolutions = async function()
 {
-    let speciesId = await this.GetSpecies();
-    let evolutionData = await fetch(speciesId.evolution_chain.url);
-    //console.log(evolutionData);
-    evolutionData = await evolutionData.json();
-    return evolutionData;
+    let chain = [];
+    let species = await this.GetSpecies();
+    let evoChain = await fetch(species.evolution_chain.url);
+    evoChain = await evoChain.json();
 }
 
 //start actual pokedex
@@ -109,19 +143,19 @@ document.getElementById("submit").addEventListener("click", () =>
     (async () =>
     {
         //wait until we get a fresh pokemon from the API
-        let newPokemon;
-        newPokemon = await Pokemon.FetchPokemon(input.value)
+        let thisPokemon;
+        thisPokemon = await Pokemon.FetchPokemon(input.value)
 
         //we have retrieved the data of a new pokemon, now we can use this data for our pokedex
         //USE POKEMON DATA IN POKEDEX HERE
-        console.log(newPokemon.name);
-        console.log(newPokemon.id);
+        console.log(thisPokemon.name);
+        console.log(thisPokemon.id);
         //DONE USING POKEMON DATA FOR POKEDEX
 
         //get previous evolution of this pokemon
         //if there is no previous evolution, catch the exception and handle it.
         let prevPokemon;
-        await newPokemon.GetPrevEvolution()
+        await thisPokemon.GetPrevEvolution()
             .then((newPokemon) =>
             {
                 prevPokemon = newPokemon;
@@ -133,9 +167,22 @@ document.getElementById("submit").addEventListener("click", () =>
             {
                 console.log("no prior evolution!");
             });
-
         console.log("...executing next job!");
 
+        //get full evolution chain of this pokemon
+        // await newPokemon.GetEvolutionChain()
+        //     .then((chain) =>
+        //     {
+        //         console.log(chain);
+        //
+        //     })
+        //     .catch((err) =>
+        //     {
+        //         console.error("oops!");
+        //     });
+
+        //return four moves of the pokemon in question
+        console.log(thisPokemon.GetMoves(4,false));
     })();
 
 
